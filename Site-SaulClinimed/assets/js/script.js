58596d34a9a30a6503624c6227cebde9a2d04e11
CassiaElen <kindraozark@gmail.com>
2023-06-21 01:02:54 -0300

    “ProjetoJava”

---
+++ Site-SaulClinimed/assets/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('.carousel').slick({
+      dots: true, // Mostrar os pontos de navegação
+      infinite: true, // Navegação infinita
+      slidesToShow: 1, // Quantidade de slides a serem mostrados ao mesmo tempo
+      slidesToScroll: 1 // Quantidade de slides a serem percorridos ao navegar
+    });
+  });
+  
